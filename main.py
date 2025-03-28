6597601c73870c7001cd9a2582aeaf32f9273515
Le Hoang <lehoangnguyen510@gmail.com>
2025-03-28 14:53:58 +0100

    prepare data

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import random
 
 import torch
+from sklearn.model_selection import train_test_split
 
 def set_seed(seed):
     random.seed(seed)
@@ -36,7 +37,36 @@ def get_device():
         print("\n=== MEMORY USAGE ===")
         print(f"Allocated: {torch.cuda.memory_allocated() / 1024**3:.2f} GB")
         print(f"Cached: {torch.cuda.memory_reserved() / 1024**3:.2f} GB")
+    
+def prepare_dataset(folder_path):
+    folders = [f for f in Path(folder_path).iterdir() if f.is_dir()]
+    print(folders)
+    img_paths = []
+    labels = []
+    for folder in folders:
+        for img_path in folder.iterdir():
+            img_paths.append(img_path)
+            labels.append(folder.name)
+    
+    return img_paths, labels
+
+def dataset_split(img_paths, labels):
+    val_size = 0.2
+    test_size = 0.125
+    is_shuffle = True
+    X_train, X_val, y_train, y_val = train_test_split(img_paths, 
+                                                      labels, 
+                                                      test_size=val_size, 
+                                                      stratify=labels, 
+                                                      shuffle=is_shuffle)
+    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, 
+                                                        test_size=test_size, 
+                                                        stratify=y_train, 
+                                                        shuffle=is_shuffle)
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
 
 if __name__=='__main__':
-    set_seed(42)
-    get_device()
+    # set_seed(42)
+    # get_device()
+    prepare_dataset("dataset/weather-dataset/dataset")
